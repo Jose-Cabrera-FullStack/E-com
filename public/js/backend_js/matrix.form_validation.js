f41d70a18e4bca996b77c31508f72ead8acd32ec
Jose <murdoc.jose.6@gmail.com>
2018-07-14 14:59:37 -0300

    despues de agregar el edit y el delete a las categories

--- public/js/backend_js/matrix.form_validation.js
+++ public/js/backend_js/matrix.form_validation.js
@@ -77,6 +77,29 @@ $(document).ready(function(){
 		}
 	});
 	
+	// edit Category Validation
+    $("#edit_category").validate({
+		rules:{
+			category_name:{
+				required:true
+			},
+			description:{
+				required:true,
+			},
+			url:{
+				required:true,
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
 
 	$("#number_validate").validate({
 		rules:{
@@ -133,5 +156,12 @@ $(document).ready(function(){
 			$(element).parents('.control-group').removeClass('error');
 			$(element).parents('.control-group').addClass('success');
 		}
+	});
+
+	$("#delCat").click(function(){
+		if(confirm('Are you sure that you want delete this cateogory')){
+			return true;
+		}
+		return false;
 	});
 });
